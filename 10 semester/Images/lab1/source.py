bd7c6d716ba7ad2d2f848609f733397ac977865f
Vladyslav Tsap <vldzpp@gmail.com>
2022-06-04 15:45:51 +0300

    integrate contrasting

--- 10 semester/Images/lab1/source.py
+++ 10 semester/Images/lab1/source.py
@@ -4,20 +4,22 @@ from threading import Thread
 import cv2
 
 from lab1.task1 import balance_brightness
+from lab1.task2 import linear_contrasting
 
 INPUT_PATH = 'input'
 OUTPUT_PATH = 'output'
 
 
-def process_image(root_path, image_name):
-    original_image = cv2.imread(os.path.join(root_path, f'{image_name}.jpg'))
-
+def process_image_brightness(original_image, image_name):
     brightness_image = balance_brightness(original_image)
-
-    cv2.imwrite(os.path.join(OUTPUT_PATH, f'{image_name}_original.jpg'), original_image)
     cv2.imwrite(os.path.join(OUTPUT_PATH, f'{image_name}_brightness.jpg'), brightness_image)
+    print(f'brightness — {image_name} done')
 
-    print(f'{image_name} processed')
+
+def process_image_contrasting(original_image, image_name):
+    contrasting_image = linear_contrasting(original_image)
+    cv2.imwrite(os.path.join(OUTPUT_PATH, f'{image_name}_contrasting.jpg'), contrasting_image)
+    print(f'contrasting — {image_name} done')
 
 
 for root, _, files in os.walk(INPUT_PATH):
@@ -26,10 +28,23 @@ for root, _, files in os.walk(INPUT_PATH):
         if file.startswith('.'):
             continue
 
+        filename = file.split('.')[0]
+
+        image = cv2.imread(os.path.join(root, f'{file}'))
+        cv2.imwrite(os.path.join(OUTPUT_PATH, f'{filename}_original.jpg'), image)
+
         Thread(
-            target=process_image,
+            target=process_image_brightness,
             kwargs={
-                'root_path': root,
-                'image_name': file.split('.')[0],
+                'original_image': image,
+                'image_name': filename,
+            },
+        ).start()
+
+        Thread(
+            target=process_image_contrasting,
+            kwargs={
+                'original_image': image,
+                'image_name': filename,
             },
         ).start()
