da60e1ad86a9fd958314133b84bcdf2defdab0f1
Vladyslav Tsap <vldzpp@gmail.com>
2022-06-04 17:24:12 +0300

    add scoring to all processes

--- 10 semester/Images/lab1/source.py
+++ 10 semester/Images/lab1/source.py
@@ -6,36 +6,57 @@ import cv2
 
 from lab1.task1 import balance_brightness
 from lab1.task2 import linear_contrasting, gamma_correction, histogram_equalisation
+from utils import calculate_psnr, calculate_ssim
 
 INPUT_PATH = 'input'
 OUTPUT_PATH = 'output'
 
 
 def process_image_brightness(image, image_name):
-    brightness_image = balance_brightness(image)
-    cv2.imwrite(os.path.join(OUTPUT_PATH, f'{image_name}_brightness.jpg'), brightness_image)
-    print(f'brightness — {image_name} done')
+    original_image = deepcopy(image)
+    balance_brightness(image)
+    cv2.imwrite(os.path.join(OUTPUT_PATH, f'{image_name}_brightness.jpg'), image)
+    print(
+        f'{image_name.upper()} — BRIGHTNESS | '
+        f'PSNR: {round(calculate_psnr(original_image, image))} | '
+        f'SSIM: {round(calculate_ssim(original_image, image))}'
+    )
 
 
 def process_image_linear_contrasting(image, image_name):
-    contrasting_image = linear_contrasting(deepcopy(image))
-    cv2.imwrite(os.path.join(OUTPUT_PATH, f'{image_name}_contrasting_linear.jpg'), contrasting_image)
-    print(f'linear contrasting — {image_name} done')
+    original_image = deepcopy(image)
+    linear_contrasting(image)
+    cv2.imwrite(os.path.join(OUTPUT_PATH, f'{image_name}_contrasting_linear.jpg'), image)
+    print(
+        f'{image_name.upper()} — LINEAR CONTRASTING | '
+        f'PSNR: {round(calculate_psnr(original_image, image))} | '
+        f'SSIM: {round(calculate_ssim(original_image, image))}'
+    )
 
 
 def process_image_gamma_correction(image, image_name):
-    contrasting_image = gamma_correction(deepcopy(image))
-    cv2.imwrite(os.path.join(OUTPUT_PATH, f'{image_name}_contrasting_gamma.jpg'), contrasting_image)
-    print(f'gamma contrasting — {image_name} done')
+    original_image = deepcopy(image)
+    gamma_correction(image)
+    cv2.imwrite(os.path.join(OUTPUT_PATH, f'{image_name}_contrasting_gamma.jpg'), image)
+    print(
+        f'{image_name.upper()} — GAMMA CORRECTION | '
+        f'PSNR: {round(calculate_psnr(original_image, image))} | '
+        f'SSIM: {round(calculate_ssim(original_image, image))}'
+    )
 
 
 def process_image_histogram_equalisation(image, image_name):
-    contrasting_image = histogram_equalisation(deepcopy(image))
-    cv2.imwrite(os.path.join(OUTPUT_PATH, f'{image_name}_contrasting_histogram.jpg'), contrasting_image)
-    print(f'histogram histogram — {image_name} done')
+    original_image = deepcopy(image)
+    histogram_equalisation(image)
+    cv2.imwrite(os.path.join(OUTPUT_PATH, f'{image_name}_contrasting_histogram.jpg'), image)
+    print(
+        f'{image_name.upper()} — HISTOGRAM EQUALIZATION | '
+        f'PSNR: {round(calculate_psnr(original_image, image))} | '
+        f'SSIM: {round(calculate_ssim(original_image, image))}'
+    )
 
 
-if __name__ == '__main__':
+def main():
     for root, _, files in os.walk(INPUT_PATH):
         for file in files:
 
@@ -56,3 +77,7 @@ if __name__ == '__main__':
             Process(target=process_image_linear_contrasting, kwargs=deepcopy(kwargs)).start()
             Process(target=process_image_gamma_correction, kwargs=deepcopy(kwargs)).start()
             Process(target=process_image_histogram_equalisation, kwargs=deepcopy(kwargs)).start()
+
+
+if __name__ == '__main__':
+    main()
